1e307a80911b36fdbafec47c2aedbd9af0139f18
Kusum Ghimire <kusum.ghimire22@gmail.com>
2021-11-13 20:02:28 +0545

    navdropdown is added to navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+ 
+import React from "react";
+
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <div className="App">
       <h1>This is the test</h1>
+      <Navbar />
     </div>
   );
 }
