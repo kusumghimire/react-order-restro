113e4c7109172e01ee2b7f76bfd95eaca5c3b4dd
Kusum Ghimire <kusum.ghimire22@gmail.com>
2021-11-13 21:26:45 +0545

    login page added to pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import React from "react";
 
 import Navbar from "./component/Navbar";
+import LoginPage from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <LoginPage />
     </div>
   );
 }
